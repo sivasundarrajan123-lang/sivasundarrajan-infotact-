8d4d7bb5c5a1d6c0371694c89e05b87967a4bdbe
Siva Sundar Rajan <sivasundarrajan70485@gmail.com>
2026-07-18 10:15:08 +0530

    Enhance analysis report with detailed summary

--- src/variable_extractor.py
+++ src/variable_extractor.py
@@ -4,20 +4,30 @@ import builtins
 def extract_variables(tree):
     variables = set()
 
-    # Built-in function names (print, len, etc.)
     builtin_names = set(dir(builtins))
 
-    # Function names
     function_names = {
         node.name for node in ast.walk(tree)
         if isinstance(node, ast.FunctionDef)
     }
+
+    imported_names = set()
+
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Import):
+            for alias in node.names:
+                imported_names.add(alias.asname or alias.name)
+
+        elif isinstance(node, ast.ImportFrom):
+            for alias in node.names:
+                imported_names.add(alias.asname or alias.name)
 
     for node in ast.walk(tree):
         if isinstance(node, ast.Name):
             if (
                 node.id not in builtin_names
                 and node.id not in function_names
+                and node.id not in imported_names
                 and node.id != "self"
             ):
                 variables.add(node.id)
--- tests/sample.py
+++ tests/sample.py
@@ -4,14 +4,29 @@ import os
 name = "Siva"
 age = 22
 
+
 class Student:
     def __init__(self):
         self.name = name
 
+    def study(self):
+        print("Studying...")
+
+
 def greet():
-    print("Hello")
+    print("Hello, Welcome to PyChronicle!")
+
 
 for i in range(3):
     greet()
+
+
+while age < 23:
+    age += 1
+
+
+if age >= 23:
+    greet()
+
 
 print(math.sqrt(25))
